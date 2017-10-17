ee7642431a43c3dad05f105be5365325fd63f0e8
Sam Mortimer <sam@mortimer.me.uk>
2017-10-16 23:02:47 -0700

    axolotl: Remove unused TARGET_USES_UNCOMPRESSED_KERNEL
    Background: https://review.lineageos.org/#/c/192397/
    
    Change-Id: If29a558950802f3be3c6f08cc67b979cd9995b1c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,7 +54,6 @@ else
     TARGET_KERNEL_CONFIG := lineage_axolotl_defconfig
 endif
 TARGET_KERNEL_SOURCE := kernel/shift/sdm845
-TARGET_USES_UNCOMPRESSED_KERNEL := false
 
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     MKDTIMG=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/libufdt/mkdtimg \
