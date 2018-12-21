070437a4771e4d6c59ecd2f6930a384d0fc1befd
Wei Wang <wvw@google.com>
2018-12-21 11:58:40 -0800

    axolotl: insmod.sh: change to use modprobe to speed up
    Before:
    [    1.474180] init: starting service 'insmod_sh'...
    [    4.342094] init: Service 'insmod_sh' (pid 524) exited with status 0
    
    After:
    [    1.489060] init: starting service 'insmod_sh'...
    [    2.286619] init: Service 'insmod_sh' (pid 524) exited with status 0
    
    Bug: 117496327
    Fixes: 121384699
    Test: boot and check modules loaded by /proc/modules
    Change-Id: I3580b6b7868dd48ab1a8ac4a65682d832375048e

--- rootdir/bin/init.insmod.sh
+++ rootdir/bin/init.insmod.sh
@@ -1,23 +1,22 @@
 #!/vendor/bin/sh
 
-#########################################
-### init.insmod.cfg format:           ###
-### --------------------------------- ###
-### [insmod|setprop] [path|prop name] ###
-### ...                               ###
-#########################################
-
-set -e
+########################################################
+### init.insmod.cfg format:                          ###
+### -----------------------------------------------  ###
+### [insmod|setprop|enable/moprobe] [path|prop name] ###
+### ...                                              ###
+########################################################
 
 cfg_file="/vendor/etc/init.insmod.cfg"
 
 if [ -f $cfg_file ]; then
-  while IFS=" " read -r action name
+  while IFS="|" read -r action arg
   do
     case $action in
-      "insmod") insmod $name ;;
-      "setprop") setprop $name 1 ;;
-      "enable") echo 1 > $name ;;
+      "insmod") insmod $arg ;;
+      "setprop") setprop $arg 1 ;;
+      "enable") echo 1 > $arg ;;
+      "modprobe") modprobe -a -d /vendor/lib/modules $arg ;;
     esac
   done < $cfg_file
 fi
@@ -25,3 +24,4 @@ fi
 # set property even if there is no insmod config
 # as property value "1" is expected in early-boot trigger
 setprop vendor.all.modules.ready 1
+setprop vendor.all.devices.ready 1
