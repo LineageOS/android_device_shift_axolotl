044750d2a8dbef939b7b7b5ae369b6a35280d611
Yingjie Wang <yingjiewang@codeaurora.org>
2020-09-04 11:51:54 +0800

    axolotl: gps: Reset cached NetworkInfo when AFW shutdown
    Reset all the cached NetworkInfo in SystemStatus as
    disconnected when framework shutdown.
    
    Change-Id: I4b8fdea07bd705479a98de8f3ab606a246e153f5
    CRs-fixed: 2771438

--- gps/location/location_interface.h
+++ gps/location/location_interface.h
@@ -98,6 +98,7 @@ struct GnssInterface {
     uint32_t (*configLeverArm)(const LeverArmConfigInfo& configInfo);
     uint32_t (*configRobustLocation)(bool enable, bool enableForE911);
     bool (*isSS5HWEnabled)();
+    void (*resetNetworkInfo)();
 };
 
 struct BatchingInterface {
