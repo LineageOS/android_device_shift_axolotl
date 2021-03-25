c63b6c16db8dd08b1556f8c1550015fcac4bf662
Edwin Wong <edwinwong@google.com>
2021-03-25 00:19:58 -0700

    axolotl: Update clearkey plugin to drm HAL v1.4
    Test: atest
      atest CtsMediaTestCases:android.media.cts.MediaDrmClearkeyTest#testGetPlaybackComponent
    
    Bug: 182525516
    Change-Id: I3adf55199301768bacb38103c3ef4ba24807df2f

--- device.mk
+++ device.mk
@@ -176,7 +176,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl:64 \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.3-service.clearkey \
+    android.hardware.drm@1.4-service.clearkey \
 
 # Fastbootd
 PRODUCT_PACKAGES += \
