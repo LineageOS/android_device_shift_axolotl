ad79b7510d609ad2cebfce824041110e01c2ee3d
linjoey <linjoey@google.com>
2021-06-16 21:29:00 +0800

    axolotl: Modify Vulkan and OpenGL version to 2020-03-01
    Bug: 191252896
    Test: android.graphics.cts.VulkanDeqpLevelTest#testVulkanDeqpLevel pass
    Change-Id: I0e598ee8a26cd614dc5076a87c514756ba39d740

--- device.mk
+++ device.mk
@@ -241,8 +241,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml \
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.software.opengles.deqp.level-2021-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
-    frameworks/native/data/etc/android.software.vulkan.deqp.level-2021-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml \
+    frameworks/native/data/etc/android.software.opengles.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
+    frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml \
 
 # Fstab
 PRODUCT_PACKAGES += \
