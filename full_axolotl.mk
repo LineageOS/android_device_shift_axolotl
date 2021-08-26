b6c114784162a4a2669994c3e60ec4f360b6414c
Alexander Martinz <amartinz@shiftphones.com>
2021-08-26 17:27:03 +0200

    axolotl: do not inherit GSI product config and add fingerprint override
    Change-Id: I09cec5ecb0ca7deded61941c1fad43be73668029
    Co-authored-by: Joey <joey@lineageos.org>
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- full_axolotl.mk
+++ full_axolotl.mk
@@ -1,18 +1,8 @@
 #
-# Copyright 2021 SHIFT GmbH
-# Copyright 2021 The LineageOS Project
+# Copyright (C) 2021 SHIFT GmbH
+# Copyright (C) 2021 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 # Inherit from those products. Most specific first.
@@ -34,8 +24,10 @@ PRODUCT_MANUFACTURER := SHIFT
 PRODUCT_GMS_CLIENTID_BASE := android-malata
 PRODUCT_SHIPPING_API_LEVEL := 29
 
-PRODUCT_BUILD_PRODUCT_IMAGE  := false
-PRODUCT_BUILD_PRODUCT_SERVICES_IMAGE := false
+# ShiftOS - 3.3 G (20210726)
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=axolotl \
+    PRODUCT_NAME=axolotl \
+    PRIVATE_BUILD_DESC="axolotl-user 10 QKQ1.201126.002 20210726 release-keys"
 
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
-PRODUCT_USE_DYNAMIC_PARTITION_SIZE := true
+BUILD_FINGERPRINT := SHIFT/axolotl/axolotl:10/QKQ1.201126.002/20210726:user/release-keys
