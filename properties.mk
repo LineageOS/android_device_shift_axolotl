2eb917bf829869802631bc73ab149d06723097b3
Alexander Martinz <amartinz@shiftphones.com>
2021-09-10 16:38:01 +0200

    axolotl: properties: remove privapp permissions property
    This is already specified in vendor/lineage and was only used
    during bringup.
    
    Change-Id: I02b7c36ab36a58539055b4706f965e9b0c661c7c
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- properties.mk
+++ properties.mk
@@ -13,10 +13,3 @@ PRODUCT_PROPERTY_OVERRIDES  += \
     dalvik.vm.heaptargetutilization=0.5 \
     dalvik.vm.heapminfree=32m \
     dalvik.vm.heapmaxfree=64m \
-
-# privapp-permissions whitelisting
-ifeq ($(TARGET_BUILD_VARIANT),eng)
-    PRODUCT_PROPERTY_OVERRIDES += ro.control_privapp_permissions=log
-else
-    PRODUCT_PROPERTY_OVERRIDES += ro.control_privapp_permissions=enforce
-endif
