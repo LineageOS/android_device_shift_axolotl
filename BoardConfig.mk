6c8576cf71d7dfad05d003b9f37baddf7dc82185
Alexander Martinz <amartinz@shiftphones.com>
2021-09-23 20:14:05 +0200

    axolotl: disable console
    This is a leftover from stock.
    
    Change-Id: Ide8e7431b8c3c70944063b6fb95286b6398da7ea
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -60,7 +60,7 @@ BOARD_KERNEL_PAGESIZE    := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
 BOARD_RAMDISK_OFFSET     := 0x02000000
 
-BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xA84000 androidboot.console=ttyMSM0 printk.devkmsg=on
+BOARD_KERNEL_CMDLINE := androidboot.console=ttyMSM0 printk.devkmsg=on
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom video=vfb:640x400,bpp=32,memsize=3072000 androidboot.configfs=true loop.max_part=7
 BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
 BOARD_KERNEL_CMDLINE += ehci-hcd.park=3
