9c2e68729dc9c82af27737bd816154a011f38162
Alexander Martinz <amartinz@shiftphones.com>
2021-12-20 09:26:53 +0100

    (DNM) axolotl: bringup for lineage-19.0
    This is a huge mess, needs to be cleaned up a lot but this
    gets the initial boot done.
    
    Change-Id: I6ba1866994b01e36b9638de0efa2d82d86ec5172
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- full_axolotl.mk
+++ full_axolotl.mk
@@ -5,13 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Enable updating of APEXes.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 # Inherit from axolotl device.
 $(call inherit-product, device/shift/axolotl/device.mk)
 
