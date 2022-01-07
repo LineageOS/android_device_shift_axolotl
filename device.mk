11652e498353eca34d2f86d9f22c19a932134774
Alexander Martinz <amartinz@shiftphones.com>
2022-01-07 13:01:20 +0100

    axolotl: Switch to Audio HAL V6
    Bug: 141989952
    Test: atest VtsHalAudioV6_0TargetTest
          atest VtsHalAudioEffectV6_0TargetTest
                manual audio "smoke" test
    
    Change-Id: I114b94a574b58d8b6c686abe4b6ff03850229957
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- device.mk
+++ device.mk
@@ -67,10 +67,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0.vendor \
     android.hardware.audio@4.0.vendor \
-    android.hardware.audio@5.0-impl \
+    android.hardware.audio@5.0.vendor \
+    android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@2.0.vendor \
     android.hardware.audio.effect@4.0.vendor \
-    android.hardware.audio.effect@5.0-impl \
+    android.hardware.audio.effect@5.0.vendor \
+    android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
     android.hardware.soundtrigger@2.2-impl \
     audio.a2dp.default \
