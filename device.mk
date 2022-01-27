30899accf53b9ce4de31bb4048455bef5ab86a19
Alexander Koskovich <akoskovich@pm.me>
2022-01-27 14:23:56 -0700

    axolotl: Drop support for Snapdragon Camera.
     * Won't be implemented this cycle (or ever again hopefully).
    
    Change-Id: Ie2a346319dfe814deaf9833cb31640505cc6b7dc

--- device.mk
+++ device.mk
@@ -143,9 +143,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service_64 \
     vendor.qti.hardware.camera.device@1.0.vendor \
 
-PRODUCT_PACKAGES += \
-    Snap \
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/camera/camxoverridesettings.txt:$(TARGET_COPY_OUT_VENDOR)/etc/camera/camxoverridesettings.txt \
 
