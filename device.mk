21a9e3d29f6355e38c314552af0e9455f40544a5
Etienne Ruffieux <eruffieux@google.com>
2022-03-04 13:27:02 +0000

    axolotl: Removed audio A2dp and Hearing Aid from device.mk
    amartinz: we are not shipping audio.hearing_aid.default
    
    Bug: 220306859
    Test: atest
    Tag: #feature
    Change-Id: Ia1d4fbe0e406e895d295936c524c5dbba6c269cd

--- device.mk
+++ device.mk
@@ -77,7 +77,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.audio.service \
     android.hardware.soundtrigger@2.2-impl \
-    audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
     libaudioalsa \
