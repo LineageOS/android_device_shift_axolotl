5676689502adeded11b21027eb9c0f1da0371d0f
Bruno Martins <bgcngm@gmail.com>
2022-09-14 14:51:15 +0100

    axolotl: Migrate to AIDL ClearKey DRM HAL
    Change-Id: Iad5cc1176978137c647069197b33f49fb4d98ab2

--- device.mk
+++ device.mk
@@ -168,7 +168,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
     android.hardware.drm@1.4.vendor \
-    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm-service.clearkey \
 
 # Fastbootd
 PRODUCT_PACKAGES += \
