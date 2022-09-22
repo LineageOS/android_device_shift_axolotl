85f3b2f48b4a639da1fb3d5a3be2a650ef8ce5a1
Alexander Martinz <amartinz@shiftphones.com>
2022-09-22 17:27:45 +0200

    axolotl: cmdline: drop virtual framebuffer setup
    This was used for early bringup purposes.
    
    Change-Id: Iba90ad4fe5ff9ffdf75755c00c43defdb6cb4b1d
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -58,7 +58,7 @@ ifeq ($(TARGET_BUILD_VARIANT),eng)
     BOARD_KERNEL_CMDLINE += console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xA84000
 endif
 BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0 printk.devkmsg=on
-BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000 androidboot.configfs=true loop.max_part=7
+BOARD_KERNEL_CMDLINE += androidboot.configfs=true loop.max_part=7
 BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
 BOARD_KERNEL_CMDLINE += ehci-hcd.park=3
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
