2adf437b0ef7f98a2d210755ec2e0f97bbb3bee6
Michael Bestas <mkbestas@lineageos.org>
2022-10-24 01:45:54 +0300

    axolotl: Build android.hidl.base@1.0 shim from source
    Change-Id: Id4604dd2d5da0643683d01ec289b2eb5ce5485c0

--- device.mk
+++ device.mk
@@ -272,6 +272,7 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.allocator@1.0.vendor \
+    android.hidl.base@1.0.vendor \
     android.hidl.memory@1.0.vendor \
     libhidltransport \
     libhidltransport.vendor \
