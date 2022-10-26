222cdccef1253b830bc9e7fa43f267a5d3a229bf
Michael Bestas <mkbestas@lineageos.org>
2022-10-26 13:09:52 +0300

    axolotl: Remove unused libcutils-v29.so copies
    Change-Id: I217fa1c4af9e26e3d416113abcc214c2d158cb62

--- device.mk
+++ device.mk
@@ -470,10 +470,6 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-service \
 
 # VNDK
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v29/arm64/arch-arm-armv8-a/shared/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libcutils-v29.so \
-    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/libcutils-v29.so \
-
 PRODUCT_PACKAGES += \
     libcomparetf2 \
     libgui_vendor \
