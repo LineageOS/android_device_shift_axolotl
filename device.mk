64a292f62149629a874ccd944594d93a418f6e3d
Alexander Martinz <amartinz@shiftphones.com>
2022-12-21 22:41:32 +0100

    axolotl: drm: switch to makefile includes
    Include widevine, if it exists.
    
    Change-Id: I88c2bbde39e528c7a5114e6f23b214467f04e579
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- device.mk
+++ device.mk
@@ -186,7 +186,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
     android.hardware.drm@1.4.vendor \
-    android.hardware.drm-service.clearkey \
+
+include frameworks/av/drm/mediadrm/plugins/clearkey/service.mk
+-include vendor/widevine/service.mk
 
 # Fastbootd
 PRODUCT_PACKAGES += \
