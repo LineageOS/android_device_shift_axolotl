802131018d9ea28b71db3a2f8bc121a7c9166973
Alexander Martinz <amartinz@shiftphones.com>
2023-01-18 12:09:37 +0100

    axolotl: shims: add IMS shim and prevent adding duplicate deps
    Change-Id: I37f5e9fe6962d17d8ac84a4f5889120c9af4d5f5
    Co-authored-by: Erfan Abdi <erfangplus@gmail.com>
    Co-authored-by: Marc Bourgoin <themard85@gmail.com>
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- device.mk
+++ device.mk
@@ -432,6 +432,8 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_PACKAGES += \
     ims-ext-common \
     ims_ext_common.xml \
+    libgui_shim \
+    libui_shim \
     qti-telephony-hidl-wrapper \
     qti_telephony_hidl_wrapper.xml \
     qti-telephony-utils \
