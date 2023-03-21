c3ebe8fdddc033cc0999c73e85b56fd66712e475
Alexander Martinz <amartinz@shiftphones.com>
2023-03-21 11:44:17 +0100

    Revert "axolotl: Migrate to AIDL ClearKey DRM HAL"
    This reverts commit c36e6d403c08e0b3bc54974b30c631abef8f1420.
    
    Test: run gts -m GtsMediaTestCases -t com.google.android.media.gts.MediaDrmTests#testWidevineBasicOperation
    
    Change-Id: I7128f5b5ff673e08c4c8e62792d16db470ea6507
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- device.mk
+++ device.mk
@@ -179,7 +179,7 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
-    android.hardware.drm-service.clearkey \
+    android.hardware.drm@1.4-service.clearkey \
 
 # Fastbootd
 PRODUCT_PACKAGES += \
