30a3ffcd0702da81b404ffb9fc2318e0f9abecfb
Gabriel Biren <gbiren@google.com>
2023-03-27 22:33:41 +0000

    axolotl: Update makefile to use the AIDL Wifi Vendor HAL.
    Bug: 274964641
    Test: Pre-submit tests
    Change-Id: I4d9114377fe75b593bbfe6a59f03d8f5d607d6a1

--- device.mk
+++ device.mk
@@ -517,7 +517,7 @@ PRODUCT_COPY_FILES += \
 
 # WiFi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     hostapd \
     libcld80211 \
     libwifi-hal-qcom \
