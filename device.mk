4ac0aa283d4781a8c4500c21701c447746674ef2
Michael Bestas <mkbestas@lineageos.org>
2023-05-29 21:32:43 +0300

    axolotl: Move some init scripts to device tree
    chmod -X rootdir/bin/*.sh while we are at it.
    
    Change-Id: Ic05c4372269f0c41981b40938f7cc85dff013820

--- device.mk
+++ device.mk
@@ -321,6 +321,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.qcom.rc \
     init.qcom.early_boot.sh \
+    init.qcom.post_boot.sh \
+    init.qcom.sh \
     init.qcom.usb.sh \
     init.recovery.qcom.rc \
     init.target.performance.rc \
