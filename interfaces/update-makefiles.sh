abe12a692f414e83b8c3f53cd5c60d4e0b5fd83a
Alexander Martinz <amartinz@shiftphones.com>
2023-06-05 13:17:52 +0200

    axolotl: lights: drop HIDL implementation
    Test: atest VtsHalLightTargetTest
    
    Change-Id: Icba381d600b427260df99a26fa0e3c43e0d2e93b
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- interfaces/update-makefiles.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
-
-do_makefiles_update \
-  "hardware.shift:device/shift/axolotl/interfaces" \
-  "android.hardware:hardware/interfaces" \
-  "android.hidl:system/libhidl/transport"
-
