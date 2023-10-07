46d1a68d382efe86a50641d5ccae02ba6779823f
basamaryan <basam.aryan@gmail.com>
2023-10-07 15:17:04 -0400

    axolotl: Build libprotobuf-cpp-full-3.9.1-vendorcompat
    For now this is implicitly built by lineage specific
    libprotobuf-cpp-lite-3.9.1-vendorcompat lib, but in the future this
    might not be the case.
    
    Change-Id: I724e933e046cbf631c078321d55550b96ca998da

--- device.mk
+++ device.mk
@@ -187,6 +187,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
     android.hardware.drm-service.clearkey \
+    libprotobuf-cpp-full-3.9.1-vendorcompat \
     libprotobuf-cpp-lite-3.9.1-vendorcompat \
 
 # Fastbootd
