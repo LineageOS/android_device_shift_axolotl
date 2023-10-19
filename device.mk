4e047b964c959f997c6864d76db0810206777eb6
LuK1337 <priv.luk@gmail.com>
2023-10-20 00:53:15 +0200

    axolotl: Build libprotobuf-cpp-lite-3.9.1-vendorcompat
    Change-Id: I0aac742db9c2b4b3e48ee20158f19405863893c2

--- device.mk
+++ device.mk
@@ -187,6 +187,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
     android.hardware.drm-service.clearkey \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat \
 
 # Fastbootd
 PRODUCT_PACKAGES += \
