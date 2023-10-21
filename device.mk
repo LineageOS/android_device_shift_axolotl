2e3df32ac39b3f3673ba89dcd6b45237bcf232ed
LuK1337 <priv.luk@gmail.com>
2023-10-21 16:34:40 +0200

    axolotl: Build android.frameworks.sensorservice@1.0.vendor
    Required by mm-pp-dpps.
    
    Change-Id: Iaf821dcbb07d7820f2fc3ff5ca9cca5e973360f2

--- device.mk
+++ device.mk
@@ -460,6 +460,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors@2.0.vendor \
     libsensorndkbridge \
 
