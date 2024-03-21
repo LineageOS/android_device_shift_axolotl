fd25b1795b43780d4ed48d5e843a6d9c8034776b
Michael Bestas <mkbestas@lineageos.org>
2024-03-21 13:38:44 +0200

    axolotl: Switch to android.hardware.usb@1.3-service.dual_role_usb
    Change-Id: Ibd14f7d31dfaf41e040302eda3bc49f351deb618

--- device.mk
+++ device.mk
@@ -488,10 +488,6 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti \
 
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service \
-
 # Update engine
 PRODUCT_PACKAGES += \
     update_engine \
@@ -503,8 +499,7 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service \
-    libusb \
+    android.hardware.usb@1.3-service.dual_role_usb \
 
 # Vibrator
 PRODUCT_PACKAGES += \
