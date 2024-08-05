83088a3fe6ca10f50030736d73b238a840541fda
Alexander Martinz <amartinz@shiftphones.com>
2024-08-05 09:18:12 +0200

    axolotl: keep using OMX
    We are in a weird situation, where we are a 64 bit only device
    with support for OMX (even if this is hacky).
    
    In future we need to move away from OMX to C2 or revert back
    to a 32+64 configuration, but for now to not break this branch
    keep the status quo.
    
    Reference: https://review.lineageos.org/399102
    
    Change-Id: I8af292800ce7cc1f0c4940b7fb1980d9712d7b1d
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- device.mk
+++ device.mk
@@ -407,6 +407,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_PRODUCT)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf \
 
+# OMX
+TARGET_SUPPORTS_OMX_SERVICE := true
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
