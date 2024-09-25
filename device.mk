d9d217491303fdd435dbe50467d3611ad7039611
Michael Bestas <mkbestas@lineageos.org>
2024-09-26 01:39:32 +0300

    axolotl: Switch to AIDL thermal HAL
    Change-Id: I6ae621194044ab66fad69b9ef997b3b347bf6120

--- device.mk
+++ device.mk
@@ -419,7 +419,7 @@ PRODUCT_BOOT_JARS += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.qti \
+    android.hardware.thermal-service.qti \
 
 # Update engine
 PRODUCT_PACKAGES += \
