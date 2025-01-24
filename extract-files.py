71e850a13de7a7913dc7b879cd53acbeed3cedcf
Nolen Johnson <johnsonnolen@gmail.com>
2025-01-24 15:14:09 -0500

    axolotl: Enable ELF checks for libantradio
    Change-Id: I163a43ec4529f2fe5eb565a8e544cd4884f41a26

--- extract-files.py
+++ extract-files.py
@@ -50,6 +50,8 @@ blob_fixups: blob_fixups_user_type = {
     'system_ext/lib64/lib-imsvideocodec.so': blob_fixup()
         .add_needed('libgui_shim.so')
         .add_needed('libui_shim.so'),
+    ('system_ext/lib/libantradio.so', 'system_ext/lib64/libantradio.so'): blob_fixup()
+        .add_needed('libnativehelper_shim.so'),
     'vendor/lib/libmmcamera_faceproc.so': blob_fixup()
         .clear_symbol_version('__aeabi_memcpy')
         .clear_symbol_version('__aeabi_memset')
