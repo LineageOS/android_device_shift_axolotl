9614c277ed5dc9f86cdc76912c6b0384eb8a807a
Alexander Martinz <amartinz@shiftphones.com>
2025-02-06 13:48:34 +0100

    axolotl: enable e-SIM support
    Change-Id: Ib111d397ae87ded93a228673988f5f7f6ab5a5cd
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- device.mk
+++ device.mk
@@ -172,6 +172,18 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey \
 
+# e-SIM
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/euicc/default-permissions_com.google.android.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/default-permissions/default-permissions_com.google.android.euicc.xml \
+    $(LOCAL_PATH)/configs/euicc/privapp_allowlist_com.google.android.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp_allowlist_com.google.android.euicc.xml \
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.telephony.euicc.xml \
+
+# Allow users to retain eSIM profiles after factory reset of user data
+PRODUCT_PRODUCT_PROPERTIES += \
+    masterclear.allow_retain_esim_profiles_after_fdr=true \
+
 # Fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
