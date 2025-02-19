27a77a8cccf2731b585de5d054462e462a810b9b
Cosmin Tanislav <demonsingur@gmail.com>
2025-02-19 02:44:05 +0200

    axolotl: switch to common QCOM AIDL bootctrl HAL
    Change-Id: I04ab771d3b1c38b58913607fbff1bb3b55e1fe25

--- device.mk
+++ device.mk
@@ -131,11 +131,8 @@ PRODUCT_PACKAGES += \
 
 # Boot control
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl-qti \
-    android.hardware.boot@1.1-impl-qti.recovery \
-    android.hardware.boot@1.1-service \
-    bootctrl.sdm845 \
-    bootctrl.sdm845.recovery \
+    android.hardware.boot-service.qti \
+    android.hardware.boot-service.qti.recovery \
 
 PRODUCT_PACKAGES_DEBUG += \
     bootctl \
